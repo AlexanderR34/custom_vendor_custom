ec844ddfdc0429386bc55d4b3f1a68d2c7add540
AlexanderR34 <oharol888@gmail.com>
2026-09-10 17:44:32 -0600

    vendor_custom: Update version to Project Diva 1.1.2 and fix display ID formatting

--- config/version.mk
+++ config/version.mk
@@ -1,6 +1,7 @@
 CUSTOM_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
 
 CUSTOM_PLATFORM_VERSION := 17.0
+DIVA_VERSION := 1.1.2
 
 CUSTOM_VERSION := $(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
 CUSTOM_VERSION_PROP := seventeen
@@ -9,10 +10,11 @@ CUSTOM_VERSION_PROP := seventeen
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.custom.build.date=$(CUSTOM_BUILD_DATE) \
     ro.custom.device=$(CUSTOM_BUILD) \
-    ro.custom.version=Project_Diva_$(CUSTOM_VERSION) \
+    ro.custom.version=Project Diva $(DIVA_VERSION) \
+    ro.diva.version=Project Diva $(DIVA_VERSION) \
     ro.build.id=Project_Diva \
     ro.system.build.id=Project_Diva \
-    ro.build.display.id=$(BUILD_ID)\\nProject_Diva_$(CUSTOM_VERSION) \
+    ro.build.display.id=Project Diva $(DIVA_VERSION) \
     net.pixelos.version=$(CUSTOM_VERSION_PROP)
 
 # Updater
