9095bcdd0b6217b4d6f417fc66cfa1ef84d00050
AlexanderR34 <oharol888@gmail.com>
2026-09-15 01:30:55 +0000

    build: Include version (1.1.2) in generated zip package name
    Change-Id: Ie2eb3d0970baa5b9358b4b76441657448626598b

--- build/tasks/pixelos.mk
+++ build/tasks/pixelos.mk
@@ -16,7 +16,7 @@
 # -----------------------------------------------------------------
 # PixelOS OTA update package
 
-CUSTOM_TARGET_PACKAGE := $(PRODUCT_OUT)/Project_diva-$(CUSTOM_BUILD_DATE).zip
+CUSTOM_TARGET_PACKAGE := $(PRODUCT_OUT)/Project_diva-$(DIVA_BASE_VERSION)-$(CUSTOM_BUILD_DATE).zip
 
 MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
 
