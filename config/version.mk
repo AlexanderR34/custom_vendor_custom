9eeca7c487f6aae33d4765c0e3119cc22230c443
AlexanderR34 <oharol888@gmail.com>
2026-09-21 17:55:20 +0000

    vendor: add dynamic UI sound themes and custom ringtones
    Change-Id: I8b3bacba7ef585d085e2f5b1dda223ba32e611f3

--- config/version.mk
+++ config/version.mk
@@ -18,7 +18,6 @@ CUSTOM_VERSION_PROP := seventeen
 # Project Diva Platform Version - Reviviendo el BP4A con elegancia
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.custom.build.date=$(CUSTOM_BUILD_DATE) \
-
     ro.custom.device=$(CUSTOM_BUILD) \
     ro.custom.version=$(CUSTOM_VERSION) \
     ro.custom.maintainer=$(DIVA_MAINTAINER) \
